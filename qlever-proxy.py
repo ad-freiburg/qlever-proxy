2e868b1388d6a81098faeb08fd1bb4da660ae463
Hannah Bast <bast@informatik.uni-freiburg.de>
2020-09-19 21:50:00 +0200

    Blocking version of proxy works now
    It receives a message from the caller, forwards it to the specified
    backend and forwards the result from the backend to the caller
    
    If something goes wrong along the way, a 404 is sent to the caller

--- qlever-proxy.py
+++ qlever-proxy.py
@@ -7,8 +7,14 @@ Author: Hannah Bast <bast@cs.uni-freiburg.de>
 import sys
 import logging
 import http.server
-import urllib
+import urllib3
 import re
+
+from urllib3 import HTTPSConnectionPool, Retry, make_headers
+from urllib3.util import Timeout
+from urllib3.exceptions import MaxRetryError
+import certifi
+
 
 """ Global log """
 log = logging.getLogger("proxy logger")
@@ -19,24 +25,126 @@ handler.setFormatter(logging.Formatter(
 log.addHandler(handler)
    
 
+class QLeverBackend:
+    """
+    Class for asking queries to one or several of the QLever backend.
+
+    NOTE: Also the backend is now HTTPS, so we need to do some additional work
+    with certificates here. We also had to do this for eval.py from the QLever
+    evaluation, where a first version of this code has been copied from.
+    """
+
+    def __init__(self, host, port, base_path):
+        """
+        Create HTTP connection pool for asking request to
+        http(s):<host>:<port>/<base_path>
+        """
+
+        self.host = host
+        self.port = port
+        self.base_path = base_path
+
+        # Retry on 404 and 503 messages because these seem to happen sometimes,
+        # but very rarely.
+        # TODO: do we need this here? I copied it from eval.py from the Qlever
+        # evaluation
+        retry = Retry(total=0, status_forcelist=[404, 503], backoff_factor=0.1)
+        # Set timeout object
+        timeout_seconds = 5
+        timeout = Timeout(connect=timeout_seconds, read=timeout_seconds)
+        # What exactly is a connection pool? Why do we need a pool? The number 4
+        # comes from eval.py
+        max_pool_size = 4
+        self.connection_pool = HTTPSConnectionPool(
+            self.host, port=self.port, maxsize=max_pool_size,
+            timeout=Timeout(connect=timeout_seconds, read=timeout_seconds),
+            retries=Retry(total=0, status_forcelist=[404, 503], backoff_factor=0.1),
+            cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        log.info("Will be forwarding requests to %s:%d%s" % (host, port, base_path))
+
+    def query(self, query_path):
+        """ 
+        Sent a GET request to the QLever backend, with the given path.
+
+        NOTE: The path is the part after GET starting with /
+        """
+
+        # params = { "cmd": "clearcachecomplete" if clearPinned else "clearcache" }
+    
+        full_path = self.base_path + query_path
+        log.info("Sending GET request to backend: %s" % full_path)
+
+        headers = make_headers(keep_alive=False)  # , fields=params
+        try:
+            response = self.connection_pool.request('GET', full_path, headers=headers)
+            assert(response.status == 200)
+            log.debug("Response data : %s", response.data \
+                if len(response.data) < 50 else str(response.data)[:47] + "...")
+            log.debug("Content type  : %s", response.getheader("Content-Type"))
+            log.debug("All headers   : %s", response.getheaders())
+            return response
+        except Exception as e:
+            log.error("Error with request to %s" % self.host)
+            log.error("Headers: %s.", response.headers)
+            log.error("Data: %s.", response.data)
+            return None
+
+
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     """
-    Class for handling GET requests to the proxy. The requests come from the
-    QLever UI have the form ...
+    Class for handling GET requests to the proxy. Currently all requests are
+    simply forwarded to a QLever backend.
     """
 
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+    def __init__(self, request, client_address, server):
+        """
+        NOTE: BaseHTTPRequestHandler has three arguments, which need to be
+        repeated here if we want to do additional stuff in the __init__. Also
+        note that the call to super().__init__ must come at the end because
+        (confusingly) do_GET is called during BaseHTTPRequestHandler.__init__
+        """
+
+        self.qlever = QLeverBackend("qlever.cs.uni-freiburg.de", 443, "/api/wikidata")
+        super().__init__(request, client_address, server)
+
+    def log_message(self, format_string, *args):
+        """ We do our own logging. """
+
+        log.debug(format_string % args)
+
 
     def do_GET(self):
+        # Process request.
         path = str(self.path)
         headers = re.sub("\n", " | ", str(self.headers))
-        log.info("GET request: %s" % path)
-        log.info("Headers: %s" % headers)
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        log.info("")
+        log.info("Received GET request: %s" % path)
+        log.debug("Headers: %s" % headers)
+
+        # Send request to QLever backend. Returns None if something went wrong,
+        # otherwise tuple of data and content type.
+        response = self.qlever.query(path)
+
+        # CASE 1: No response, send 404.
+        if response == None:
+            self.send_response(404)
+            self.end_headers()
+            log.info("Proxy sent 404")
+        # CASE 2: Proper response received, just forward it.
+        else:
+            self.send_response(200)
+            # Forward selected headers.
+            # NOTE: without the Access-Control-Allow-Origin, the QLever UI will
+            # not accept the result.
+            headers_preserved = ["Content-Type", "Access-Control-Allow-Origin"]
+            for header_key in headers_preserved:
+                header_value = response.getheader(header_key)
+                if header_value != None:
+                    self.send_header(header_key, header_value)
+            self.end_headers()
+            self.wfile.write(response.data)
+            log.info("Forwarded result from backend to caller"
+                    ", headers preserved: %s" % ", ".join(headers_preserved))
 
 
 def server_loop(hostname, port):
@@ -47,7 +155,8 @@ def server_loop(hostname, port):
     """
 
     server_address = (hostname, port)
-    server = http.server.HTTPServer(server_address, RequestHandler)
+    request_handler = RequestHandler
+    server = http.server.HTTPServer(server_address, request_handler)
     log.info("Listening to GET requests on %s:%d" % (hostname, port))
     server.serve_forever()
 
